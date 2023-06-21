3c10f03401f37d790aba4723678f5b9b6a63d266
qvantt <2275399@gmail.com>
2023-06-21 11:58:37 +0300

    add single tags

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,18 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
+
   # Your code goes here...
+  class Tag
+    def self.build(tag, options = {})
+      result = []
+      result << tag
+      result << build_options(options) if options.any?
+      "<#{result.join(options.any? ? " " : "")}>"
+    end
+
+    def self.build_options(opts)
+      opts.map { |key, value| "#{key}=\"#{value}\"" }
+    end
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -10,4 +10,14 @@ class TestHexletCode < Minitest::Test
   def test_it_does_something_useful
     assert true
   end
+
+  def test_it_makes_br_tag
+    tag = HexletCode::Tag.build("br")
+    assert_equal("<br>", tag)
+  end
+
+  def test_it_makes_img_tag
+    tag = HexletCode::Tag.build("img", src: "path/to/image")
+    assert_equal('<img src="path/to/image">', tag)
+  end
 end
