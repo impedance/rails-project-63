90c31d3ee8549fde91a5ac10382324390817bc37
qvantt <2275399@gmail.com>
2023-06-26 15:14:35 +0300

    add comment

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,19 +1,21 @@
-  # frozen_string_literal: true
-  module Tag
-    SINGLE_TAGS = %w[input img br].freeze
+# frozen_string_literal: true
 
-    def self.build(tag, attrs = {})
-      store = []
-      store << tag
-      store << build_attrs(attrs) if attrs.any?
-      prepared_tags = "<#{store.join(attrs.any? ? " " : "")}>"
+# This module provides methods for building HTML
+module Tag
+  SINGLE_TAGS = %w[input img br].freeze
 
-      return prepared_tags if SINGLE_TAGS.include?(tag)
+  def self.build(tag, attrs = {})
+    store = []
+    store << tag
+    store << build_attrs(attrs) if attrs.any?
+    prepared_tags = "<#{store.join(attrs.any? ? " " : "")}>"
 
-      "#{prepared_tags}#{yield if block_given?}</#{tag}>"
-    end
+    return prepared_tags if SINGLE_TAGS.include?(tag)
 
-    def self.build_attrs(attrs)
-      attrs.map { |key, value| "#{key}=\"#{value}\"" }
-    end
+    "#{prepared_tags}#{yield if block_given?}</#{tag}>"
   end
+
+  def self.build_attrs(attrs)
+    attrs.map { |key, value| "#{key}=\"#{value}\"" }
+  end
+end
